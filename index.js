35b1caa0a7bb9e394bebbe6cdffc4d291bf951fb
Mohd Sharif <itmsharif@vankukil.com>
2025-02-23 17:32:58 +0530

    Add Swagger Documentation

--- index.js
+++ index.js
@@ -2,7 +2,30 @@ import express from 'express';
 import usersRouter from './routes/users.js';
 import authRouter from './routes/auth.js';
 
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
 const app = express();
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'NodeJS API Projct For MySQL',
+      version: '1.0.0',
+      description: 'A simple user API',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+  },
+  apis: ['./routes/*.js'],
+};
+
+const specs = swaggerJSDoc(options);
+app.use('/', swaggerUi.serve, swaggerUi.setup(specs));
 
 //middleware
 app.use(express.json());
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,54 @@
-import expreess from 'express';
+import express from 'express';
 import { register, login, logout } from '../controllers/auth.js';
 
-const router = expreess.Router();
+const router = express.Router();
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      User:
+ *          type: object
+ *          required:
+ *              - username
+ *              - email
+ *              - password
+ *              - name
+ *          properties:
+ *              username:
+ *                  type: string
+ *                  description: The username
+ *              email:
+ *                  type: string
+ *                  description: The email
+ *              password:
+ *                  type: string
+ *                  description: The password
+ *              name:
+ *                  type: string
+ *                  description: The name
+ */
+
+/**
+ * @swagger
+ * /auth/register:
+ *  post:
+ *      summary: Register a new user
+ *      description: Register a new user
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/User'
+ *      responses:
+ *          200:
+ *              description: User has been created
+ */
 router.post('/register', register);
+
 router.post('/login', login);
+
 router.post('/logout', logout);
 
 export default router;
